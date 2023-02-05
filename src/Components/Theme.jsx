4209a2e3a1856b0ccc68f4a30506292191d04ebd
prem-2301 <aaronstone246@gmail.com>
2023-02-05 23:57:31 +0530

    add themes included

--- src/Components/Theme.jsx
+++ src/Components/Theme.jsx
@@ -26,12 +26,58 @@ function Theme() {
 	}
 
 	function pink() {
+		let equ = document.getElementById("equal");
+		equ.classList.toggle("pink-eq");
+		let plus = document.getElementById("plus");
+		plus.classList.toggle("pink-op");
+		let sub = document.getElementById("sub");
+		sub.classList.toggle("pink-op");
+		let mul = document.getElementById("mul");
+		mul.classList.toggle("pink-op");
+		let divi = document.getElementById("divi");
+		divi.classList.toggle("pink-op");
+		let clear = document.getElementById("clear");
+		clear.classList.toggle("pink-op");
+		let clentry = document.getElementById("clentry");
+		clentry.classList.toggle("pink-op");
 		let type = document.querySelectorAll(".keys");
 		for (let i = 0; i < type.length; i++) {
-			type[i].classList.toggle("dark-keys");
+			type[i].classList.toggle("pink-keys");
 		}
 		let area = document.querySelectorAll(".result");
-		area[0].classList.toggle("res-dark");
+		area[0].classList.toggle("res-pink");
+	}
+
+	function green() {
+		let equ = document.getElementById("equal");
+		equ.classList.toggle("green-eq");
+		let type = document.querySelectorAll(".keys");
+		for (let i = 0; i < type.length; i++) {
+			type[i].classList.toggle("green-keys");
+		}
+		let area = document.querySelectorAll(".result");
+		area[0].classList.toggle("res-green");
+	}
+
+	function whitbl() {
+		let equ = document.getElementById("equal");
+		equ.classList.toggle("whbl-op");
+		let plus = document.getElementById("plus");
+		plus.classList.toggle("whbl-op");
+		let sub = document.getElementById("sub");
+		sub.classList.toggle("whbl-op");
+		let mul = document.getElementById("mul");
+		mul.classList.toggle("whbl-op");
+		let divi = document.getElementById("divi");
+		divi.classList.toggle("whbl-op");
+		let clear = document.getElementById("clear");
+		clear.classList.toggle("whbl-clr");
+		let clentry = document.getElementById("clentry");
+		clentry.classList.toggle("whbl-clr");
+		let type = document.querySelectorAll(".keys");
+		for (let i = 0; i < type.length; i++) {
+			type[i].classList.toggle("whbl-keys");
+		}
 	}
 
 	return (
@@ -49,8 +95,18 @@ function Theme() {
 						pink();
 					}}
 				></div>
-				<div className="themes th3"></div>
-				<div className="themes th4"></div>
+				<div
+					className="themes th3"
+					onClick={() => {
+						green();
+					}}
+				></div>
+				<div
+					className="themes th4"
+					onClick={() => {
+						whitbl();
+					}}
+				></div>
 			</div>
 		</div>
 	);
